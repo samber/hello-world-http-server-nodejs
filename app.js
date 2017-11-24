791ba44d43736233417f04e8ddaa40467c4997ea
Samuel BERTHE ツ <dev@samuel-berthe.fr>
2017-11-24 15:09:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = parseInt(process.env.PORT) || 8080;
+const name = parseInt(process.env.NAME) || "fail";
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => res.send('Hello ' + name))
 
 app.listen(port, () => console.log('Example app listening on port ' + port))
